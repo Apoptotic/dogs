f0430399ddd2410515ccbf9152257b965901f7f9
Mihai Popa <apoptotic23@gmail.com>
2020-03-31 14:39:43 +0300

    Search autocomple from material UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
 import Header from "./modules/header/Header";
 import Home from "./modules/home/Home";
 import Footer from "./modules/footer/Footer";
 
 function App() {
   return (
-    <>
+    <StylesProvider injectFirst>
       <Header />
       <Switch>
         <Route path="/breed-list">
@@ -17,7 +18,7 @@ function App() {
         </Route>
       </Switch>
       <Footer />
-    </>
+    </StylesProvider>
   );
 }
 
--- src/modules/header/Header.js
+++ src/modules/header/Header.js
@@ -2,12 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
+import Search from "./Search";
 
 function Header() {
   return (
     <StyledHeader>
       <Logo />
       <NavBar />
+      <StyledSearchContainer>
+        <Search />
+      </StyledSearchContainer>
     </StyledHeader>
   );
 }
@@ -21,6 +25,15 @@ const StyledHeader = styled.header`
   @media screen and (max-width: 750px) {
     justify-content: start;
   }
+`;
+
+const StyledSearchContainer = styled.div`
+  width: 15%;
+  position: absolute;
+  top: 50%;
+  right: 3%;
+  transform: translateY(-50%);
+  background-color: #ffffff;
 `;
 
 export default Header;
